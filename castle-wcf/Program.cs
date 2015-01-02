ddb8f3eb580e81c7e18a85c2121027f78ee7bdea
michal6 <michalduda01@gmail.com>
2015-01-02 09:37:21 +0100

    example project to reproduce castle wcf issue

---
+++ castle-wcf/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Castle.Facilities.TypedFactory;
+using Castle.Facilities.WcfIntegration;
+using Castle.MicroKernel.Registration;
+using Castle.Windsor;
+using Castle.Facilities.TypedFactory;
+using Component = Castle.MicroKernel.Registration.Component;
+
+namespace ConsoleApplication7
+{
+    public interface IHelloService
+    {
+        string Hello();
+    }
+
+    public class HelloService : IHelloService
+    {
+
+        public string Hello()
+        {
+            return "Hello";
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var container = new WindsorContainer()
+                .AddFacility<WcfFacility>()
+                .Register(
+                    Component.For<IHelloService>().ImplementedBy<HelloService>());
+
+            var service = new DefaultServiceHostFactory().CreateServiceHost(typeof(IHelloService).AssemblyQualifiedName, new Uri[0]);
+            service.Open();
+
+            Console.ReadKey();
+        }
+    }
+}
